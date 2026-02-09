40af2679d50b2746040888ea7984ab4ebf74323a
vdiamar2309 <vdiamar2309>
2026-02-09 09:27:33 +0100

    new file:   .vscode/launch.json 	deleted:    javascript/Examen LM Javascript 25-26/README 	new file:   javascript/Examen LM Javascript 25-26/index.html 	new file:   javascript/Examen LM Javascript 25-26/script.js

---
+++ javascript/Examen LM Javascript 25-26/script.js
@@ -0,0 +1,43 @@
+// CE.c
+
+let btnadd = document.getElementById(btnAdd);
+let btnborrar = document.getElementById(btnDelete);
+let textos = document.getElementById(texto);
+let btnoscuro = document.getElementById(btnDark);
+let listas = document.getElementById(lista);
+let titulo = document.getElementById(titulo);
+
+
+
+
+// CE.d
+addEventListener(array.forEach(textos => {}));
+
+
+if (textos==""){
+    alert("No se puede dejar el texto en blanco")
+    
+} 
+
+
+
+
+
+
+
+qu
+
+// CE.e
+
+
+
+
+
+
+
+
+
+
+// CE.f
+
+c
